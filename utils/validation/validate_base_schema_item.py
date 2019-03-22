b33916b6d77e5f24b532ccfd8305ac1e42ac4526
Abhay Sharma <abhay.bits@gmail.com>
2019-03-22 21:52:52 +0530

    Changing paths from test to master (prepare for merge)

--- utils/validation/validate_base_schema_item.py
+++ utils/validation/validate_base_schema_item.py
@@ -8,12 +8,15 @@ cwd = os.getcwd()
 base_dir_abs_path = cwd+'/'
 
 if (len(sys.argv[1:])==0):
-  print "Usage: python jsonschema_val.py rel-path-to-item [rel-path-to-root-dir]"
+  print "Usage: python jsonschema_val.py rel-path-to-item abs-rel-switch [rel-path-to-root-dir]"
   exit()
 elif (len(sys.argv[1:])==1):
+  print "Usage: python jsonschema_val.py rel-path-to-item abs-rel-switch [rel-path-to-root-dir]"
+  exit()
+elif (len(sys.argv[1:])==2):
   root_dir_path = cwd+'/'
 else:
-  root_dir_path = os.path.join(base_dir_abs_path, sys.argv[2])+'/'
+  root_dir_path = os.path.join(base_dir_abs_path, sys.argv[3])+'/'
 
 
 # Your data
@@ -24,23 +27,22 @@ with open(data_path) as data_object:
 relative_schema_file_path = data['refBaseSchema'].split('/')[-3] + '/'+data['refBaseSchema'].split('/')[-2]+'/'+data['refBaseSchema'].split('/')[-1]
 
 #If required to run the validation script using absolute path
-#path_for_schema = data['refBaseSchema'].split('/')[-3] + '/'+data['refBaseSchema'].split('/')[-2]+'/'
-#schema_url = data['refBaseSchema']
-#schema_name = data['refBaseSchema'].split('/')[-1]
-#with open(wget.download(schema_url),'r') as fobj:
-#   schema = json.load(fobj)
-#os.remove(cwd+'/'+schema_name)
-
-schema_path = relative_schema_file_path
-schema_file = os.path.join(root_dir_path + schema_path)
-print "-------------------------------------------------------------------------------------------\n"
-print "Json item     :"+ sys.argv[1] + '\n'
-print "Schema File   :" + relative_schema_file_path
-print "\n-------------------------------------------------------------------------------------------\n"
-
-with open(schema_file) as file_object:
-    schema = json.load(file_object)
-
+if (sys.argv[2] == "0"):
+   path_for_schema = data['refBaseSchema'].split('/')[-3] + '/'+data['refBaseSchema'].split('/')[-2]+'/'
+   schema_url = data['refBaseSchema']
+   schema_name = data['refBaseSchema'].split('/')[-1]
+   with open(wget.download(schema_url),'r') as fobj:
+      schema = json.load(fobj)
+   os.remove(cwd+'/'+schema_name)
+else :
+   schema_path = relative_schema_file_path
+   schema_file = os.path.join(root_dir_path + schema_path)
+   print "-------------------------------------------------------------------------------------------\n"
+   print "Json item     :"+ sys.argv[1] + '\n'
+   print "Schema File   :" + relative_schema_file_path
+   print "\n-------------------------------------------------------------------------------------------\n"
+   with open(schema_file) as file_object:
+       schema = json.load(file_object)
 
 
 # Note that the second parameter does nothing.
