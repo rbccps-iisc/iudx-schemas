01f30e9bc98741ecc1ccfd3e0392f213bf39d6f9
Abhay Sharma <abhay.bits@gmail.com>
2019-03-23 13:28:57 +0530

    Few modifications to change_branch_paths script and moved it to utils folder

--- utils/misc/change_branch_paths.sh
+++ utils/misc/change_branch_paths.sh
@@ -2,6 +2,12 @@
 
 #grep -R "githubusercontent.com"  --include \*.json -l
 
+if [ "$#" -lt 2 ] ; then
+    echo "Replace search-pattern with replace-pattern in all the json files in the given sub-directory"
+    echo "Usage: $0 <search-pattern> <replace-pattern>"
+    exit 1
+fi
+
 var1=$(echo "$1" | sed 's/\//\\\//g')
 var2=$(echo "$2" | sed 's/\//\\\//g')
 while read line ;
